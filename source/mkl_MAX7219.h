cba5db3ed696da1ff84400362f5d30cb9be9a00c
ThiagoCosta <tcaa@icomp.ufam.edu.br>
2020-03-24 18:50:30 -0400

    feat: class comments

--- source/mkl_MAX7219.h
+++ source/mkl_MAX7219.h
@@ -1,3 +1,44 @@
+/*!
+ * @copyright   © 2020 UFAM - Federal University of Amazonas.
+ *
+ * @brief       Implementation of the class "mkl_MAX7219".
+ *
+ * @file        mkl_MAX7219.cpp
+ * @version     1.0
+ * @date        March 24, 2020
+ *
+ * @section     HARDWARES & SOFTWARES
+ *              +board        FRDM-KL25Z da NXP.
+ *              +processor    MKL25Z128VLK4 - ARM Cortex-M0+.
+ *              +compiler     MCUXpresso Integrated Development Environment (IDE)
+ *              +manual       L25P80M48SF0RM, Rev.3, September 2012.
+ *              +revisions    Version (date): Brief description.
+ *                             ++ 1.0 (March 24, 2020): Initial version.
+ *
+ * @section     AUTHORS & DEVELOPERS
+ *              +institution  Federal University of Amazonas.
+ *              +courses      Computer Engineering / Electrical Engineering.
+ *              +teacher      Miguel Grimm <miguelgrimm@gmail.com>
+ *              +student      Initial version:
+ *                             ++ Thiago Costa Antunes Afonso <tcaa@icomp.ufam.edu.br>
+ *
+ * @section     LICENSE
+ *
+ *              GNU General Public License (GNU GPL).
+ *
+ *              This program is free software; You can redistribute it
+ *              and/or modification in accordance with the terms of the "GNU General Public
+ *              License" published by the Free Software Foundation; Be a
+ *              version 3 of the license or any later version.
+ *
+ *              This program is distributed in the hope that it will be useful,
+ *              but WITHOUT ANY WARRANTY; Without the implied warranty of
+ *              MARKETING OR USE FOR A PARTICULAR PURPOSE.
+ *              See the "GNU General Public License" website for more details.
+ *
+ * @htmlonly    http://www.gnu.org/copyleft/gpl.html
+ */
+
 #ifndef MKL_MAX7219_H_
 #define MKL_MAX7219_H_
 
@@ -25,7 +66,6 @@ typedef enum
 class mkl_MAX7219
 {
 private:
-
     /* The array for shifting the data to the devices */
     uint8_t spidata[16];
     /* Send out a single command to the device */
@@ -38,7 +78,6 @@ private:
     /* This one is driven LOW for chip selection */
     mkl_GPIOPort SPI_CS;
 
-    
 public:
     explicit mkl_MAX7219(gpio_Pin dataPin, gpio_Pin clkPin, gpio_Pin csPin);
 
@@ -51,7 +90,7 @@ public:
     */
     void writeCode(uint8_t msb, uint8_t lsb);
 
-        /* 
+    /* 
     * Set the number of digits (or rows) to be displayed.
     * See datasheet for sideeffects of the scanlimit on the brightness
     * of the display.
@@ -74,7 +113,6 @@ public:
     *           for normal operation.
     */
     void shutdown(bool status);
-
 };
 
 #endif //  MKL_MAX7219_H_
